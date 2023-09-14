bd7677b740ee02d32564688728d2167bf84ee808
Shahadat Hosen <shahadat74238@gmail.com>
2023-09-14 23:05:05 +0600

    Header Complect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      
-      <h1 className='text-center'>Course Registration</h1>
+      <Header></Header>
      
     </>
   )
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-center text-4xl font-bold my-12">Course Registration</h1>
+    </header>
+  );
+};
+
+export default Header;
